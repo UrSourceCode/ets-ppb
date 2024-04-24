19df2cc4284fb5df799cb7ef5243f61053b1878a
elshiraphine <erviana.205025@mhs.its.ac.id>
2024-04-24 16:41:36 +0700

    fix: modify main page

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:ets_ppb/page/edit_films_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'page/films_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static String title = 'Films SQLite';
+
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: title,
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(
+        primaryColor: Colors.black,
+        scaffoldBackgroundColor: Colors.blueGrey.shade900,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+      ),
+      home: const FilmsPage(),
+    );
+  }
+}
